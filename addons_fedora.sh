821878eae3b9ee1694419572769e41b8be182675
Cornel Panceac <cpanceac@gmail.com>
2021-07-01 12:58:30 +0300

    Adding youtube-dl to addons script.

--- addons_fedora.sh
+++ addons_fedora.sh
@@ -6,7 +6,7 @@ A=(supertux smc megamario kdegames3 gnome-nibbles ksnakeduel aisleriot gnome-204
      gnome-mines kmines gtetrinet kpat gnome-mahjongg kmahjongg kshisen gnome-sudoku 
      ksudoku kigo ccgo gnome-chess knights BlockOutII ltris
      inkscape mypaint gimp blender avidemux easytag id3lib clementine amarok k3b dvdstyler imagination gwenview 
-     gcc-c++ codeblocks geany mkvtoolnix-gui darktable stellarium celestia wine)
+     gcc-c++ codeblocks geany mkvtoolnix-gui darktable stellarium celestia wine yotube-dl)
 
 dnf -y install ${A[@]}
 
